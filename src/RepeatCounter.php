72c92fb9c6f199b1681aaa2bbe3a6033d20e52de
GStewartN <stewart.nathang@gmail.com>
2017-07-07 09:27:13 -0700

    add char validation test - fail

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,6 +1,9 @@
 <?php
     class RepeatCounter
     {
-      
+        function countRepeats($input1, $input2)
+        {
+          
+        }
     }
 ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -3,6 +3,18 @@
 
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
-      
+        function testCountRepeatsCharValid()
+        {
+            //Arrange
+            $test_RepeatCounter = new RepeatCounter;
+            $input1 = "fe53#";
+            $input2 = "bfd437&";
+
+            //Act
+            $result = $test_RepeatCounter->countRepeats($input1, $input2);
+
+            //Assert
+            $this->assertEquals("Please enter letters only!", $result);
+        }
     }
 ?>
